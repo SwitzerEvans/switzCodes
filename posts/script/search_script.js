b04f9d849353a81830c32ba3c9348fdcee056aa8
SwitzerEvans <chiemekaevans928@gmail.com>
2025-11-11 17:01:44 +0100

    Update search_script.js

--- posts/script/search_script.js
+++ posts/script/search_script.js
@@ -27,104 +27,139 @@ closeSearch.addEventListener('click', function () {
 const posts = [
     {
         title: "Introduction to HTML",
-        link: "./Introduction_to_HTML_My_First_Step_Into_Web_Development.html",
+        link: "https://switzcodes.com/posts/EvansPosts-HTML/Introduction_to_HTML_My_First_Step_Into_Web_Development.html",
         description: "My First Step Into Web Development"
     },
     {
         title: "Understanding the HTML Document Structure",
-        link: "./Understanding_the_HTML_Document_Structure.html",
+        link: "https://switzcodes.com/posts/EvansPosts-HTML/Understanding_the_HTML_Document_Structure.html",
         description: "Building the Foundation of Every Web Page"
     },
     {
         title: "Headings and Paragraphs",
-        link: "./HTML_Text_Formatting.html",
+        link: "https://switzcodes.com/posts/EvansPosts-HTML/HTML_Text_Formatting.html",
         description: "Writing Basic Page Content"
     },
     {
         title: "HTML Text Formatting",
-        link: "./HTML_Text_Formatting.html",
+        link: "https://switzcodes.com/posts/EvansPosts-HTML/HTML_Text_Formatting.html",
         description: "Bold, Italic, Underline and More"
     },
     {
         title: "Creating Links with the &lt;a&gt; Tag",
-        link: "./Creating_Links_with_the_a_Tag.html",
+        link: "https://switzcodes.com/posts/EvansPosts-HTML/Creating_Links_with_the_a_Tag.html",
         description: "Linking Web Pages"
     },
     {
         title: "Adding Images with &lt;img&gt;",
-        link: "./Adding_Images_with_img.html",
+        link: "https://switzcodes.com/posts/EvansPosts-HTML/Adding_Images_with_img.html",
         description: "Making Web Pages Visual"
     },
     {
         title: "Lists in HTML",
-        link: "./Lists_in_HTML.html",
+        link: "https://switzcodes.com/posts/EvansPosts-HTML/Lists_in_HTML.html",
         description: "Ordered and Unordered Lists"
     },
     {
         title: "Creating Tables",
-        link: "./Creating_Tables.html",
+        link: "https://switzcodes.com/posts/EvansPosts-HTML/Creating_Tables.html",
         description: "Organizing Data in HTML"
     },
     {
         title: "HTML Forms",
-        link: "./HTML_Forms.html",
+        link: "https://switzcodes.com/posts/EvansPosts-HTML/HTML_Forms.html",
         description: "Text, Radio, Checkbox, Button"
     },
     {
         title: "Input Types and Form Elements",
-        link: "./Input_Types_and_Form_Elements.html",
+        link: "https://switzcodes.com/posts/EvansPosts-HTML/Input_Types_and_Form_Elements.html",
         description: "Writing Basic Page Content"
     },
     {
         title: "HTML Attributes",
-        link: "./HTML_Attributes.html",
+        link: "https://switzcodes.com/posts/EvansPosts-HTML/HTML_Attributes.html",
         description: "What Makes Tags More Powerful"
     },
     {
         title: "HTML Semantic Tags",
-        link: "./HTML_Semantic_Tags.html",
+        link: "https://switzcodes.com/posts/EvansPosts-HTML/HTML_Semantic_Tags.html",
         description: "Writing Clean and Meaningful Code"
     },
     {
         title: "HTML Entities",
-        link: "./HTML_Entities.html",
+        link: "https://switzcodes.com/posts/EvansPosts-HTML/HTML_Entities.html",
         description: "Displaying Special Characters"
     },
     {
         title: "Embedding Videos and Audio",
-        link: "./Embedding_Videos_and_Audio.html",
+        link: "https://switzcodes.com/posts/EvansPosts-HTML/Embedding_Videos_and_Audio.html",
         description: "Making Pages Multimedia"
     },
     {
         title: "Using &lt;div&gt; and &lt;span&gt;",
-        link: "./Using_div_and_span.html",
+        link: "https://switzcodes.com/posts/EvansPosts-HTML/Using_div_and_span.html",
         description: "Grouping and Styling Elements"
     },
     {
         title: "How I Learned to Comment My Code in HTML",
-        link: "./How_I_Learned_to_Comment_My_Code_in_HTML.html",
+        link: "https://switzcodes.com/posts/EvansPosts-HTML/How_I_Learned_to_Comment_My_Code_in_HTML.html",
         description: "Making Code Easy to Read and Maintain"
     },
     {
         title: "HTML Meta Tags",
-        link: "./HTML_Meta_Tags.html",
+        link: "https://switzcodes.com/posts/EvansPosts-HTML/HTML_Meta_Tags.html",
         description: "Controlling How My Page Appears in Search Engines"
     },
     {
         title: "Creating Navigation Menus with HTML",
-        link: "./Creating_Navigation_Menus_with_HTML.html",
+        link: "https://switzcodes.com/posts/EvansPosts-HTML/Creating_Navigation_Menus_with_HTML.html",
         description: "Making Site Easy to Explore"
     },
     {
         title: "HTML Best Practices I’m Following as a Beginner",
-        link: "./HTML_Best_Practices_I’m_Following_as_a_Beginner.html",
+        link: "https://switzcodes.com/posts/EvansPosts-HTML/HTML_Best_Practices_I’m_Following_as_a_Beginner.html",
         description: "Simple Rules for Better Web Pages"
     },
     {
         title: "Building My First Full HTML Page",
-        link: "./Building_My_First_Full_HTML_Page.html",
+        link: "https://switzcodes.com/posts/EvansPosts-HTML/Building_My_First_Full_HTML_Page.html",
         description: "What I Learned"
-    }
+    },
+    {
+        title: "Introduction to CSS",
+        link: "https://switzcodes.com/posts/EvansPosts-CSS/Introduction_to_CSS_My_First_Step_Into_Styling_Web_Pages.html",
+        description: "My First Step Into Styling Web Pages"
+    },
+    {
+        title: "Understanding How CSS Works",
+        link: "https://switzcodes.com/posts/EvansPosts-CSS/Understanding_How_CSS_Works.html",
+        description: "From Selectors to Properties"
+    },
+    {
+        title: "Ways to Add CSS ",
+        link: "https://switzcodes.com/posts/EvansPosts-CSS/Ways_to_Add_CSS.html",
+        description: "Inline, Internal, and External Stylesheets"
+    },
+    {
+        title: "CSS Selectors",
+        link: "https://switzcodes.com/posts/EvansPosts-CSS/CSS_Selectors.html",
+        description: "Targeting the Right Elements"
+    },
+    // {
+    //     title: "Colors in CSS ",
+    //     link: "https://switzcodes.com/posts/EvansPosts-CSS/Colors_in_CSS.html",
+    //     description: "From Names to Hex, RGB, and HSL"
+    // },
+    // {
+    //     title: "Styling Text",
+    //     link: "https://switzcodes.com/posts/EvansPosts-CSS/Styling_Text.html",
+    //     description: "Fonts, Sizes, and Line Spacing"
+    // },
+    // {
+    //     title: "CSS Box Model",
+    //     link: "https://switzcodes.com/posts/EvansPosts-CSS/CSS_Box_Model.html",
+    //     description: "Margin, Border, Padding, and Content"
+    // }
 ];
 
 // searchPosts function
@@ -209,3 +244,4 @@ if (window.innerWidth < 1000) {
 // if(window.innerWidth >= 1000) {
 //     mobileSearchContainer.style.display = 'none';
 // }
+
